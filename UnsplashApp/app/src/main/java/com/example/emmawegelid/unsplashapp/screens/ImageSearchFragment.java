6852a9f3ee9a381d43996afec295207557bde5bc
emmawegelid <emma@frost.se>
2018-04-05 17:31:36 +0200

    Extracting bar title to resources

--- UnsplashApp/app/src/main/java/com/example/emmawegelid/unsplashapp/screens/ImageSearchFragment.java
+++ UnsplashApp/app/src/main/java/com/example/emmawegelid/unsplashapp/screens/ImageSearchFragment.java
@@ -96,7 +96,7 @@ public class ImageSearchFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        getActivity().setTitle("Image search");
+        getActivity().setTitle(getString(R.string.image_search_title));
     }
 
     @Override
